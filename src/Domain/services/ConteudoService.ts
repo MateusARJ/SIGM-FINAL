0e1f5e5c01623d213fc5168aba639908e83be0cb
Vitor Lopes <vcoder00@gmail.com>
2026-01-21 10:26:06 -0300

    fix: modificação no ConteudoService para integração

--- src/Domain/services/ConteudoService.ts
+++ src/Domain/services/ConteudoService.ts
@@ -1,8 +1,13 @@
-// Facade para o serviço de Conteúdo
 import { IConteudoService } from "../interfaces/IConteudoService";
 import { IRepository } from "../interfaces/IRepository";
 import { SolicitacaoConteudo, RegistroConteudo } from "../Models/RequisicaoModelo";
-import { CriarConteudoUseCase } from "../usecases/CriarConteudoUsecase";
+
+import { ObterConteudoUseCase } from "../usecases/ObterConteudoUseCase";
+import { VerificarStatusUseCase } from "../usecases/VerificarStatusUseCase";
+import { EditarConteudoUseCase } from "../usecases/EditarConteudoUseCase";
+import { ExcluirConteudoUseCase } from "../usecases/ExcluirConteudoUseCase";
+import { CriarConteudoUseCase } from "../usecases/CriarConteudoUseCase";
+
 import { v4 as uuidv4 } from 'uuid';
 import { IIAClient } from "../interfaces/IIAClienteService";
 
@@ -71,12 +76,84 @@ import { IIAClient } from "../interfaces/IIAClienteService";
 //     return {requestId};
 //   }
 // }
+// export class ConteudoService implements IConteudoService {
+//   constructor(
+//     private criarConteudo: CriarConteudoUseCase, // injetado o usecase específico
+//     private repo: IRepository
+//   ) {}
+
+//   /**
+//    * Facade para criar solicitação
+//    * 
+//    * ela chama o usecase especifico para criação de conteúdo e devolve requestId
+//    */
+//   criarSolicitacao(s: SolicitacaoConteudo): Promise<{ requestId: string }> {
+//     return this.criarConteudo.execute(s);
+//   }
+
+//   async obterConteudoPorId(requestId: string) {
+//         const conteudo = await this.repo.buscarConteudoPorId(requestId);
+//         if (!conteudo) throw new Error("Não encontrado");
+//         return conteudo;
+//     }
+
+//     async verificarStatusGeracao(requestId: string) {
+//         const conteudo = await this.obterConteudoPorId(requestId);
+//         return conteudo.status;
+//     }
+
+//     async editar(requestId: string, dados: Required<Pick<RegistroConteudo, 'resultado'>>) {
+//         // 1. Busca o objeto completo existente
+//         const atual = await this.obterConteudoPorId(requestId);
+
+//         // 2. Cria um NOVO objeto mesclando o antigo com as atualizações
+//         const objetoAtualizado = {
+//             ...atual,  // Espalha: id, disciplinaId, tipoConteudo, numeroSlides...
+//             ...dados,  // Sobrescreve apenas os campos que vieram na edição
+//             status: "editado" // Atualiza o status
+//         };
+
+//         // 3. Salva o objeto já mesclado  
+//         await this.repo.atualizarConteudo(objetoAtualizado);
+//         return { requestId }
+//     }
+
+//     async excluir(requestId: string) {
+//         await this.repo.removerConteudo(requestId);
+//         return { requestId };
+//     }
+// }
+
+/**
+ * Funções dessa classe são facade ou seja apontam outras funções
+ * executoras no caso os UseCases
+ */
 export class ConteudoService implements IConteudoService {
   constructor(
-    private criarConteudo: CriarConteudoUseCase
+    private criarConteudo: CriarConteudoUseCase,
+    private obterConteudo: ObterConteudoUseCase,
+    private verificarStatus: VerificarStatusUseCase,
+    private editarConteudo: EditarConteudoUseCase,
+    private excluirConteudo: ExcluirConteudoUseCase
   ) {}
 
   criarSolicitacao(s: SolicitacaoConteudo) {
     return this.criarConteudo.execute(s);
+  }
+
+  obterConteudoPorId(id: string) {
+    return this.obterConteudo.execute(id);
+  }
+
+  verificarStatusGeracao(id: string) {
+    return this.verificarStatus.execute(id);
+  }
+
+  editar(id: string, dados: Required<Pick<RegistroConteudo, 'resultado'>>) {
+    return this.editarConteudo.execute(id, dados);
+  }
+
+  excluir(id: string) {
+    return this.excluirConteudo.execute(id);
   }
 }
