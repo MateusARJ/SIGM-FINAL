b678830ef62eaf9c0af6de2d5dddc442f1123286
MateusARJ <mateusmm46532@gmail.com>
2026-01-20 21:23:11 -0300

    Feature: Adicionando a nova rota findAssuntoByDisciplina

--- src/Domain/interfaces/IAssuntoService.ts
+++ src/Domain/interfaces/IAssuntoService.ts
@@ -4,6 +4,7 @@ export interface IAssuntoService {
   list(): Promise<Assunto[]>
   get(id: string): Promise<Assunto>
   findByName(nome: string): Promise<Assunto | undefined>
+  findAssuntoByDisciplina(disciplinaId: string): Promise<Assunto[]>
   create(data: Omit<Assunto, 'id'>): Promise<Assunto>
   update(id: string, data: Partial<Omit<Assunto, 'id'>>): Promise<void>
   delete(id: string): Promise<void>
